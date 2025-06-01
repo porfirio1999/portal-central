e98b9f45fb6c2c1a8568623a91ceceb1e9daf488
porfirio1999 <114274997+porfirio1999@users.noreply.github.com>
2025-05-31 19:00:38 -0600

    Update portal_central.py

--- portal_central.py
+++ portal_central.py
@@ -49,7 +49,6 @@ def actualizar_url():
 
 # === INICIO ===
 if __name__ == '__main__':
-    if not os.path.exists('estaciones.db'):
-        with app.app_context():
-            db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=5000)
