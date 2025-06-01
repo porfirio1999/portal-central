9ede22bb74f07a7327bfa7c82712d399b8940829
porfirio1999 <114274997+porfirio1999@users.noreply.github.com>
2025-05-31 18:53:15 -0600

    Update portal_central.py

--- portal_central.py
+++ portal_central.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///estaciones.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 db = SQLAlchemy(app)
 
 # === MODELO ===
